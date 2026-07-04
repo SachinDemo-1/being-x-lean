9ac25c2158e0f9857891d3d360ae5871be431f1a
SachinDemo-1 <sachingola20077002@gmail.com>
2026-07-04 18:17:02 +0530

    Make Pdf Bar

--- frontend/src/pages/Diet.js
+++ frontend/src/pages/Diet.js
@@ -163,7 +163,7 @@ function generatePDF(meals, userInfo, macros, targetCalories, goalLabel) {
 
   // Stats box
   doc.setFillColor(28, 28, 28); doc.roundedRect(margin, 105, cW, 62, 4, 4, 'F');
-  doc.setFillColor(...gold); doc.roundedRect(margin, 105, cW, 6, 4, 4, 'F'); doc.rect(margin, 108, cW, 3, 'F');
+  doc.setFillColor(...gold); doc.roundedRect(margin, 105, cW, 9, 4, 4, 'F'); doc.rect(margin, 108, cW, 6, 'F');
   doc.setTextColor(...dark); doc.setFont('helvetica', 'bold'); doc.setFontSize(8);
   doc.text('YOUR STATS', margin + 6, 112);
   const stats = [['Weight', `${userInfo.weight}kg`], ['Height', `${userInfo.height}cm`], ['Age', `${userInfo.age}yrs`], ['Gender', userInfo.gender], ['Diet', userInfo.dietType === 'veg' ? 'Vegetarian' : 'Non-Veg'], ['Activity', userInfo.activity.replace('_', ' ')]];
@@ -176,7 +176,7 @@ function generatePDF(meals, userInfo, macros, targetCalories, goalLabel) {
 
   // Macros box
   doc.setFillColor(28, 28, 28); doc.roundedRect(margin, 178, cW, 52, 4, 4, 'F');
-  doc.setFillColor(...gold); doc.roundedRect(margin, 178, cW, 6, 4, 4, 'F'); doc.rect(margin, 181, cW, 3, 'F');
+  doc.setFillColor(...gold); doc.roundedRect(margin, 178, cW, 9, 4, 4, 'F'); doc.rect(margin, 181, cW, 6, 'F');
   doc.setTextColor(...dark); doc.setFont('helvetica', 'bold'); doc.setFontSize(8); doc.text('DAILY TARGETS', margin + 6, 185);
   [['CALORIES', `${targetCalories}`, 'kcal'], ['PROTEIN', `${macros.protein}`, 'g'], ['CARBS', `${macros.carbs}`, 'g'], ['FAT', `${macros.fat}`, 'g']].forEach(([lbl, val, unit], i) => {
     const mx = margin + 6 + i * (cW / 4);
