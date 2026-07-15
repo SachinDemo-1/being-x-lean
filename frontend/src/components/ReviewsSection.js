73dee39ad24a4975aa52d68d81a5b9282274957d
SachinDemo-1 <sachingola20077002@gmail.com>
2026-07-16 00:26:03 +0530

    Review System Update 1.0.2

--- frontend/src/components/ReviewsSection.js
+++ frontend/src/components/ReviewsSection.js
@@ -28,7 +28,7 @@ export default function ReviewsSection() {
 
   useEffect(() => {
     let cancelled = false;
-    fetchReviews(9)
+    fetchReviews(5)
       .then(data => {
         if (cancelled) return;
         if (data.reviews?.length) setReviews(data.reviews);
